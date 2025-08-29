9729f741fe1d6a2bd0e50b0631a969c5ebdb4d79
abeera5677 <hinakandiya@gmail.com>
2025-08-30 01:36:08 +0530

    Create temp_converter.py

---
+++ temp_converter.py
@@ -0,0 +1,14 @@
+# Temprature Converter: Celsius to Fahrenheit 
+
+# Take temperature input from the user in Celsius 
+celsius = float(input("Entre temprature in Celsius:"))
+
+# Convert Celsius to Fahrenheit 
+fahrenheit = (celsius * 9/5) + 32
+
+# Print the result 
+print(f"{celsius}°C is equal {fahrenheit}°F) 
+
+# Sampls Output:
+# Entre temperature in Celsius: 25
+# 25.0°C is equal to 77.0°F
