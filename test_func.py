c81924476b4817bc2a28dbbe90c81a068d22d31a
abeera5677 <hinakandiya@gmail.com>
2025-09-08 11:04:07 +0530

    Create test_func.py

---
+++ test_func.py
@@ -0,0 +1,11 @@
+def  is_even(numbers):
+     if number % 2 == 0:
+        return True
+     else:
+        return False
+print(is_even(4))          # False
+print(is_even(7))          # True
+
+# output:
+  True
+  False
